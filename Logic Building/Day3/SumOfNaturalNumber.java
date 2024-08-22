b63fd1e01c512624e8722d065f559230261d9369
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-08-22 16:08:21 +0530

    Add files via upload

---
+++ Logic Building/Day3/SumOfNaturalNumber.java
@@ -0,0 +1,11 @@
+class SumOfNaturalNumber{
+	public static void main(String[] agrs){
+		int i = 1;
+		int sum = 0;
+		for(;i < 51;i++){
+			sum = sum + i;
+		}
+		
+		System.out.println("Sum of First 50 natural number is ::" + sum);
+	}
+}
