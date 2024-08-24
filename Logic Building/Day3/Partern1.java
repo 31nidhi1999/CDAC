6e0b2a4da573172dde6d5162bb15f0f0dbcc9841
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-08-24 20:56:39 +0530

    Add files via upload

--- Logic Building/Day3/Partern1.java
+++ Logic Building/Day3/Partern1.java
@@ -9,11 +9,36 @@
 */
 class Partern1 { 
 	public static void main(String[] args) { 
-		for(int i =1; i<=6;i++){
-			for(int j =1; j<=i;j++){
-				System.out.print("*");
+	    int row = 6,count = 1;
+		int i = 1;
+		while(count<=row){
+			if(i%2==0 && i!=2){
+				i++;
+				continue;
+			}else{
+				for(int j = 1;j<=i;j++){
+					System.out.print("*");
+					
+				}
+				i++;
+				count++;
+				System.out.println();
 			}
-			System.out.println("");
+			if(count>row){
+				break;
+			}
 		}
+		
 	} 
 } 
+
+
+/*
+ count i j o/p
+ 0     1 1  *
+ 1     2 1  *
+ 1   
+
+
+
+*/
