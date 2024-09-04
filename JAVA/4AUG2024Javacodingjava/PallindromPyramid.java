3df8069b9d64c48fd59448e77860c2876381b659
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-09-04 20:39:02 +0530

    Add files via upload

--- JAVA/4AUG2024Javacodingjava/PallindromPyramid.java
+++ JAVA/4AUG2024Javacodingjava/PallindromPyramid.java
@@ -6,23 +6,35 @@ public class PallindromPyramid {
 		// TODO Auto-generated method stub
 		
 		for(int i =1;i<=5;i++) {
+			/*space*/
 			for(int j=1;j<=5-i;j++) {
 				System.out.print(" ");
 			}
-			int w = i -1;
-			for(int k =1;k<=2*i-1;k++) {
-				
-				if(k<=5) {
-					System.out.print(k);
-				}else {
-					System.out.print(w);
-					w--;
-					
-				}
+			/*
+			    1
+			   12
+			  123
+			 1234
+			12345
+ */
+			for(int k = 1;k<=i;k++) {
+				System.out.print(k);;
+			}
+			
+			/*
+			 space
+			 1
+			 21
+			 321
+			 4321*/
+			int w = i-1;
+			for(int l =1;l<i;l++) {
+				System.out.print(w);
+				w--;
 			}
 			System.out.println("");
 		}
 
 	}
-
+	
 }
