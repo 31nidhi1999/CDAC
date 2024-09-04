a12b0ccabdd258441087914ac6a265da4d6daaa3
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-09-04 15:37:55 +0530

    Add files via upload

---
+++ JAVA/Assigment2/BMICalculator.java
@@ -0,0 +1,39 @@
+/*
+ *
+ Underweight: BMI less than 18.5
+Normal weight: BMI of 18.5 to 24.9
+Overweight: BMI of 25 to 29.9
+Obesity: BMI of 30 or higher*/
+
+package assigment2;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a weight in Kg :");
+		float weight = sc.nextFloat();
+		
+		System.out.println("Enter a height in cm:");
+		float height = sc.nextFloat();
+		
+		float bmiCal = weight/(height/100 * height/100);
+		
+		System.out.println(bmiCal);
+		
+		if (bmiCal<18.5) {
+			System.out.println("Underweight");
+		}else if(bmiCal>=18.5 && bmiCal<24.9) {
+			System.out.println("Normal wieght");
+		}else if(bmiCal>=25 && bmiCal<29.9) {
+			System.out.println("Overweight");
+		}else if(bmiCal>30) {
+			System.out.println("Obesity");
+		}
+	}
+
+}
---
+++ JAVA/Assigment2/LeapYear.java
@@ -0,0 +1,31 @@
+package assigment2;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int year =  sc.nextInt();
+		char c;
+		
+		if(year % 400 ==0 ||(year %4 == 0 && year %100 !=0)) {
+			c ='a';
+		}else {
+			c ='b';
+		}
+		
+		switch (c) {
+		case 'a':
+			System.out.println(year +  " is a Leap Year");
+			break;
+		case 'b':
+			System.out.println(year + " is Not a Leap Year");
+		}
+		
+
+	}
+
+}
