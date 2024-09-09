adea634726182428d965933234ebce6279263e97
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-09-09 10:26:24 +0530

    Add files via upload

---
+++ JAVA/Assigmet4sandeepsir/CompIntrest/src/in/cdac/acts/Program.java
@@ -0,0 +1,25 @@
+package in.cdac.acts;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LoanAmortizationCalculatorUtil lacu = new LoanAmortizationCalculatorUtil();
+		
+		int choice;
+		while((choice=LoanAmortizationCalculatorUtil.menuList()) !=0) {
+			switch (choice) {
+			case 1:
+				lacu.acceptRecord();
+				break;
+			case 2:
+				lacu.printRecord();
+				break;
+			}
+			
+		}
+
+	}
+
+}
