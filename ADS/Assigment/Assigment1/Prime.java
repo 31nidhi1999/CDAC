1d068879fcd207d7c5221f9016b055d4ca0bf65f
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-09-26 01:04:07 +0530

    Add files via upload

--- ADS/Assigment/Assigment1/Prime.java
+++ ADS/Assigment/Assigment1/Prime.java
@@ -1,24 +1,26 @@
 class Prime{
 	
-	static int i=2;
 	static int  count;
-	static String isPrime(int n){
+	static Boolean isPrime(int n,int i){
 		
-		if(n%i==0 && i<n){
-			count++;
-			i++;
-			isPrime(n);
+		if(n==2) {
+					
+			return (n==2)?true:false;
+		}
+			
+		if(n%i==0){
+			return false;
 		}
 		
-		if(count == 0)
-			return (n +" is a prime number");
-		return (n +" is not a prime number");
 		
+		return isPrime(n,++i);
 	}
 	
 	public static void main(String[] args){
-		System.out.println(isPrime(7));
-		System.out.println(isPrime(10));
+		System.out.println(isPrime(29,2));
+		System.out.println(isPrime(2,2));
+		System.out.println(isPrime(4,2));
+		System.out.println(isPrime(15,2));
 	}
 	
 }
