fe85463c0dadfe91813e4e6e99fe457c2c73bd87
31nidhi1999 <49243280+31nidhi1999@users.noreply.github.com>
2024-09-29 04:09:16 +0530

    Add files via upload

---
+++ ADS/Assigment/Assigment2/ReverseArrayInPlace.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ReverseArrayInPlace{
+	public static void main(String[] agrs){
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		
+		for(int i =0;i < size;i++){
+			arr[i]=sc.nextInt();
+		}
+		
+		System.out.print("[");
+		for(int j =size - 1 ;j>=0;j--){
+			
+			if(j==0){
+				System.out.print(arr[j]);
+			}else{
+				System.out.print(arr[j]+",");
+			}
+		}
+		System.out.print("]");
+	}
+}
